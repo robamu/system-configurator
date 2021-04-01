120b5adddbfd36fa31756ca9af5544b9f29345b2
Robin Mueller <robin.mueller.m@gmail.com>
2021-04-02 01:14:04 +0200

    this will do for now

--- aliashelper.py
+++ aliashelper.py
@@ -3,6 +3,7 @@ Python utility to set up common and useful aliases
 """
 import os
 import sys
+import getpass
 
 ALIASES_FILENAME = ".bash_aliases"
 GENERIC_ALIASES = \
@@ -36,40 +37,48 @@ TEST_FILENAME = "test_aliases.txt"
 
 def main():
     if sys.platform.startswith("win32"):
-        # This is the path for git
-        os.chdir(os.getenv('userprofile'))
+        print("Detected Windows platform")
+        generate_windows_aliases()
     elif sys.platform.startswith("linux"):
-        os.chdir("~")
-    if os.path.isfile(".bash_aliases"):
+        print("Detected Linux platform")
+        generate_unix_aliases()
+    else:
+        print("Not implemented for this OS.")
+        sys.exit(0)
+
+
+def generate_windows_aliases():
+    print("Setting up aliases for git..")
+    # This is the path for git
+    os.chdir(os.getenv('userprofile'))
+    if os.path.isfile(".bash_aliases") and not TEST_MODE:
         print(f"{ALIASES_FILENAME} file already exists")
         sys.exit(0)
     aliases_string_buf = GENERIC_ALIASES
-    shortcut_alias = ""
-    if sys.platform.startswith("win32"):
-        notepad_alias = "alias notepad=\"/c/Program\ Files\ \(x86\)/Notepad++/notepad++.exe\"\n"
-        aliases_string_buf += "\n" + notepad_alias
-        shortcut_alias = SHORTCUT_ALIAS_INCOMP + f"notepad {ALIASES_FILENAME}\n"
-    elif sys.platform.startswith("linux"):
-        unix_editor = prompt_unix_editor()
-        shortcut_alias = SHORTCUT_ALIAS_INCOMP + f"{unix_editor} {ALIASES_FILENAME}\n"
-    else:
-        print("Unknown OS")
+    notepad_alias = "alias notepad=\"/c/Program\ Files\ \(x86\)/Notepad++/notepad++.exe\"\n"
+    aliases_string_buf += "\n" + notepad_alias
+    shortcut_alias = SHORTCUT_ALIAS_INCOMP + f"notepad {ALIASES_FILENAME}\n"
     aliases_string_buf += shortcut_alias
+    target_file = file_writer(ALIASES_FILENAME, aliases_string_buf)
+    print(f"Generated {target_file} in {os.getcwd()} for git")
 
-    if not TEST_MODE:
-        target_file = ALIASES_FILENAME
-        with open(target_file, "w") as file:
-            file.write(aliases_string_buf)
-    else:
-        target_file = TEST_FILENAME
-        with open(target_file, "w") as file:
-            file.write(aliases_string_buf)
+    # This is the path for MinGW64
+    username = getpass.getuser()
+    os.chdir(f"C:/msys64/home/{username}")
+    target_file = file_writer(ALIASES_FILENAME, aliases_string_buf)
+    print(f"Generated {target_file} in {os.getcwd()} for MinGW64")
 
+def generate_unix_aliases():
+    os.chdir("~")
+    if os.path.isfile(".bash_aliases") and not TEST_MODE:
+        print(f"{ALIASES_FILENAME} file already exists")
+        sys.exit(0)
+    aliases_string_buf = GENERIC_ALIASES
+    unix_editor = prompt_unix_editor()
+    shortcut_alias = SHORTCUT_ALIAS_INCOMP + f"{unix_editor} {ALIASES_FILENAME}\n"
+    aliases_string_buf += shortcut_alias
+    target_file = file_writer(ALIASES_FILENAME, aliases_string_buf)
     print(f"Generated {target_file} in {os.getcwd()}")
-
-
-if __name__ == "__main__":
-    main()
 
 
 def prompt_unix_editor() -> str:
@@ -88,3 +97,19 @@ def prompt_unix_editor() -> str:
                 return custom_editor
         elif 0 < editor_id < 3:
             return EDITOR_SELECTION[editor_id]
+
+
+def file_writer(target_filename, alias_buffer: str) -> str:
+    if not TEST_MODE:
+        target_file = target_filename
+        with open(target_file, "w") as file:
+            file.write(alias_buffer)
+    else:
+        target_file = TEST_FILENAME
+        with open(target_file, "w") as file:
+            file.write(alias_buffer)
+    return target_file
+
+
+if __name__ == "__main__":
+    main()
