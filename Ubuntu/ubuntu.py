45d5f373b0b779238a7a1505453f82b508fa7066
Robin Mueller <robin.mueller.m@gmail.com>
2021-05-18 20:04:08 +0200

    updated ubuntu script

--- Ubuntu/ubuntu.py
+++ Ubuntu/ubuntu.py
@@ -5,8 +5,9 @@ MINIMIZE_TO_DOCK_CMD = "gsettings set org.gnome.shell.extensions.dash-to-dock cl
 
 def main():
     os.system(MINIMIZE_TO_DOCK_CMD)
-    os.system("snap install spotify")
-    os.system("snap install discord")
+    os.system("sudo snap install spotify")
+    os.system("sudo snap install discord")
+	os.system("sudo snap install --classic code")
     os.system("sudo adduser $USER dialout")
     os.system("sudo apt-get install ubuntu-restricted-extras ubuntu-restricted-addons")
     os.system("sudo apt-get python3-pip")
