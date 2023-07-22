be23d73f9d48f31d3af0e02c169aecd7a47454c8
Robin Mueller <robin.mueller.m@gmail.com>
2023-07-22 19:12:26 +0200

    make LSP key bindings generic

--- unix/cfg/.config/nvim/lua/setup/rust-tools.lua
+++ unix/cfg/.config/nvim/lua/setup/rust-tools.lua
@@ -1,16 +1,7 @@
 local function on_attach(client, buffer)
   local keymap_opts = { buffer = buffer }
   -- Code navigation and shortcuts
-  vim.keymap.set("n", "<c-]>", vim.lsp.buf.definition, keymap_opts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, keymap_opts)
-  vim.keymap.set("n", "gD", vim.lsp.buf.implementation, keymap_opts)
-  vim.keymap.set("n", "<c-k>", vim.lsp.buf.signature_help, keymap_opts)
   vim.keymap.set("n", "1gD", vim.lsp.buf.type_definition, keymap_opts)
-  vim.keymap.set("n", "gr", vim.lsp.buf.references, keymap_opts)
-  vim.keymap.set("n", "g0", vim.lsp.buf.document_symbol, keymap_opts)
-  vim.keymap.set("n", "gW", vim.lsp.buf.workspace_symbol, keymap_opts)
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, keymap_opts)
-  vim.keymap.set("n", "ga", vim.lsp.buf.code_action, keymap_opts)
   -- Get signatures (and _only_ signatures) when in argument lists.
   require "lsp_signature".on_attach({
     doc_lines = 0,
