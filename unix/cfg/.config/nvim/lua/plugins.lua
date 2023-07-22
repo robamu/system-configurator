66b1e053a51b9e4b4ce4b042d118d8e0de36c1f8
Robin Mueller <robin.mueller.m@gmail.com>
2023-07-22 18:32:43 +0200

    split up lua files for cfg

--- unix/cfg/.config/nvim/lua/plugins.lua
+++ unix/cfg/.config/nvim/lua/plugins.lua
@@ -1,5 +1,3 @@
-vim.cmd [[packadd packer.nvim]]
-
 -- ensure the packer plugin manager is installed
 local ensure_packer = function()
   local fn = vim.fn
@@ -69,165 +67,15 @@ require('packer').startup(function(use)
   }
 end)
 
+
 -- the first run will install packer and our plugins
 if packer_bootstrap then
   require("packer").sync()
   return
 end
 
--- Plugin setup
-require("mason").setup()
-require("mason-lspconfig").setup()
-require("gruvbox").setup()
-
--- Avoid showing extra messages when using completion
-vim.opt.shortmess = vim.opt.shortmess + "c"
-
-local function on_attach(client, buffer)
-  local keymap_opts = { buffer = buffer }
-  -- Code navigation and shortcuts
-  vim.keymap.set("n", "<c-]>", vim.lsp.buf.definition, keymap_opts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, keymap_opts)
-  vim.keymap.set("n", "gD", vim.lsp.buf.implementation, keymap_opts)
-  vim.keymap.set("n", "<c-k>", vim.lsp.buf.signature_help, keymap_opts)
-  vim.keymap.set("n", "1gD", vim.lsp.buf.type_definition, keymap_opts)
-  vim.keymap.set("n", "gr", vim.lsp.buf.references, keymap_opts)
-  vim.keymap.set("n", "g0", vim.lsp.buf.document_symbol, keymap_opts)
-  vim.keymap.set("n", "gW", vim.lsp.buf.workspace_symbol, keymap_opts)
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, keymap_opts)
-  vim.keymap.set("n", "ga", vim.lsp.buf.code_action, keymap_opts)
-  -- Get signatures (and _only_ signatures) when in argument lists.
-  require "lsp_signature".on_attach({
-    doc_lines = 0,
-    handler_opts = {
-      border = "none"
-    },
-  })
-end
-
--- Configure LSP through rust-tools.nvim plugin.
--- rust-tools will configure and enable certain LSP features for us.
--- See https://github.com/simrat39/rust-tools.nvim#configuration
-local opts = {
-  runnables = {
-    use_telescope = true,
-  },
-  tools = {
-    inlay_hints = {
-      auto = true,
-      show_parameter_hints = false,
-      parameter_hints_prefix = "",
-      other_hints_prefix = "",
-    },
-  },
-
-  -- all the opts to send to nvim-lspconfig
-  -- these override the defaults set by rust-tools.nvim
-  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
-  server = {
-    -- on_attach is a callback called when the language server attachs to the buffer
-    on_attach = on_attach,
-    settings = {
-      -- to enable rust-analyzer settings visit:
-      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-      ["rust-analyzer"] = {
-        -- enable clippy on save
-        checkOnSave = {
-          command = "clippy",
-        },
-      },
-    },
-  },
-}
-
-require("rust-tools").setup(opts)
-
--- Setup Completion
--- See https://github.com/hrsh7th/nvim-cmp#basic-configuration
-local cmp = require("cmp")
--- local select_opts = {behavior = cmp.SelectBehaviour.Select}
-
-cmp.setup({
-  preselect = cmp.PreselectMode.None,
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  mapping = {
-    ['<Up>'] = cmp.mapping.select_prev_item(),
-    ['<Down>'] = cmp.mapping.select_next_item(),
-
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<C-y>'] = cmp.mapping.confirm({select = true}),
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-
-    ['<C-f>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(1) then
-        luasnip.jump(1)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-
-    ['<C-b>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      local col = vim.fn.col('.') - 1
-
-      if cmp.visible() then
-        cmp.select_next_item(select_opts)
-      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        fallback()
-      else
-        cmp.complete()
-      end
-    end, {'i', 's'}),
-
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item(select_opts)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-  },
-
-  -- Installed sources:
-  sources = {
-    { name = 'path' },                              -- file paths
-    { name = 'nvim_lsp', keyword_length = 3 },      -- from language server
-    { name = 'nvim_lsp_signature_help'},            -- display function signatures with current parameter emphasized
-    { name = 'nvim_lua', keyword_length = 2},       -- complete neovim's Lua runtime API such vim.lsp.*
-    { name = 'buffer', keyword_length = 2 },        -- source current buffer
-    { name = 'vsnip', keyword_length = 2 },         -- nvim-cmp source for vim-vsnip 
-    -- { name = 'calc'},                               -- source for math calculation
-  },
-})
-
--- Enable completing paths in :
-cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({
-    { name = 'path' }
-  })
-})
-
--- If you want insert `(` after select function or method item
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
-)
+require("setup/mason")
+require("setup/gruvbox")
+require("setup/rust-tools")
+require("setup/nvim-cmp")
 
---
+++ unix/cfg/.config/nvim/lua/setup/rust-tools.lua
@@ -0,0 +1,59 @@
+local function on_attach(client, buffer)
+  local keymap_opts = { buffer = buffer }
+  -- Code navigation and shortcuts
+  vim.keymap.set("n", "<c-]>", vim.lsp.buf.definition, keymap_opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, keymap_opts)
+  vim.keymap.set("n", "gD", vim.lsp.buf.implementation, keymap_opts)
+  vim.keymap.set("n", "<c-k>", vim.lsp.buf.signature_help, keymap_opts)
+  vim.keymap.set("n", "1gD", vim.lsp.buf.type_definition, keymap_opts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, keymap_opts)
+  vim.keymap.set("n", "g0", vim.lsp.buf.document_symbol, keymap_opts)
+  vim.keymap.set("n", "gW", vim.lsp.buf.workspace_symbol, keymap_opts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, keymap_opts)
+  vim.keymap.set("n", "ga", vim.lsp.buf.code_action, keymap_opts)
+  -- Get signatures (and _only_ signatures) when in argument lists.
+  require "lsp_signature".on_attach({
+    doc_lines = 0,
+    handler_opts = {
+      border = "none"
+    },
+  })
+end
+
+-- Configure LSP through rust-tools.nvim plugin.
+-- rust-tools will configure and enable certain LSP features for us.
+-- See https://github.com/simrat39/rust-tools.nvim#configuration
+local opts = {
+  runnables = {
+    use_telescope = true,
+  },
+  tools = {
+    inlay_hints = {
+      auto = true,
+      show_parameter_hints = false,
+      parameter_hints_prefix = "",
+      other_hints_prefix = "",
+    },
+  },
+
+  -- all the opts to send to nvim-lspconfig
+  -- these override the defaults set by rust-tools.nvim
+  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+  server = {
+    -- on_attach is a callback called when the language server attachs to the buffer
+    on_attach = on_attach,
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ["rust-analyzer"] = {
+        -- enable clippy on save
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
+  },
+}
+
+require("rust-tools").setup(opts)
+
